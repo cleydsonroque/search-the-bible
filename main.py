7071492a6251207fc94f8f1270552b63bd5a9523
Cleydson Roque da Silva Rezende <cleydson_roque@petrobras.com.br>
2023-04-11 18:33:31 -0300

    test sqlite

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+con = sqlite3.connect('KJF.sqlite')
+cur = con.cursor()
+#cur.execute("SELECT * FROM sqlite_master WHERE type='table' ORDER BY name")
+for row in cur.execute("SELECT text FROM verse WHERE chapter='1'"):
+    print(row)
+con.close()
+"""
+('table', 'book', 'book', 2, 'CREATE TABLE "book" (\n\t"id"\tINTEGER,\n\t"book_reference_id"\tINTEGER,\n\t"testament_reference_id"\tINTEGER,\n\t"name"\tTEXT\n)')
+('table', 'metadata', 'metadata', 3, 'CREATE TABLE "metadata" (\n\t"key"\tTEXT,\n\t"value"\tTEXT\n)')
+('table', 'testament', 'testament', 4, 'CREATE TABLE "testament" (\n\t"id"\tINTEGER,\n\t"name"\tTEXT\n)')
+('table', 'verse', 'verse', 5, 'CREATE TABLE "verse" (\n\t"id"\tINTEGER,\n\t"book_id"\tINTEGER,\n\t"chapter"\tINTEGER,\n\t"verse"\tINTEGER,\n\t"text"\tTEXT\n)')
+(31102, 66, 22, 21, 'A graça de nosso Senhor Jesus Cristo esteja com todos vós. Amém.')
+"""
